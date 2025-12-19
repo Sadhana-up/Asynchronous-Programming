495aecb595880ac08b0ee1c39162590ff203a1c2
Sadhana Uprety <upretysadhana@gmail.com>
2025-12-19 20:27:42 +0545

    added file

--- task04.py
+++ task04.py
@@ -1,6 +1,10 @@
-# Question:
-# Write a program to show the time difference between:
+# Write an async function countdown(n) that prints numbers 
+# from n to 1, waiting 1 second between each number.
+import asyncio
 
-# Running 3 async tasks one after another
+async def countdown(n):
+    for i in range(n,0,-1):
+        print(i)
+        await asyncio.sleep(1)
 
-# Running the same 3 async tasks concurrently
+asyncio.run(countdown(5))
